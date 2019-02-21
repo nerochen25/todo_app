e63944d16db03acfaab15f057f3e6ef0584c7498
Nero <nerochen25@berkeley.edu>
2019-02-20 22:01:14 -0800

    Add todo component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: "buy some milk"},
+      {id: 2, content: "play mario game"}
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="center blue-text">Nero's Todo Tracker</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
