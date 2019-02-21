c8a4f417dd4e587966008b2f2baee7485a62332e
Nero <nerochen25@berkeley.edu>
2019-02-20 22:24:05 -0800

    Add addTodo function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
   state = {
@@ -19,11 +20,20 @@ class App extends Component {
     })
   }
 
+  addTodo = (todo) => {
+    todo.id = Math.random() * 10;
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Nero's Todo Tracker</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
