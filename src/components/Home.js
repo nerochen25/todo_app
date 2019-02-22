dcff8f2a415b05458641b1ff914d508b7bdee3b0
Nero <nerochen25@berkeley.edu>
2019-02-21 20:49:56 -0800

    Create component folder and add Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="container">Home</h1>
+        <p>Software Engineer with a passion for React, Redux, ES6, and Ruby on Rails. Deciding to become a software developer was when I realized the power of programming and integrating computer languages to turn ideas into tangible and functioning softwares that could bring joy and convenience to other people's life. Beyond code, I love playing Basketball, Volleyball and Badminton with friends during my spare time. Because of sports, I learned leadership in school teams and the ability of learning things quicker and coping stress better.</p>
+      </div>
+    )
+  }
+}
